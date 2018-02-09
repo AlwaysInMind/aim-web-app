e6c2686c4f117116ced047b2d3225fa5816914ea
Steve Lee <steve@opendirective.com>
2018-02-09 21:59:36 +0000

    Add sample auth0 variables file for client

--- client/src/auth/auth0-variables-sample.js
+++ client/src/auth/auth0-variables-sample.js
@@ -1,11 +1,7 @@
-const CALLBACKPATH = '/auth/signed-in'
-
 export const AUTH0_CONFIG = {
   DOMAIN: '<DOMAIN>.auth0.com',
   CLIENTID: 'CLIENT_ID',
-  CALLBACKPATH,
-  CALLBACKURL: `${window.location.protocol}//${window.location.hostname}:${
-    window.location.port
-  }${CALLBACKPATH}`,
   APIID: '<API_ID>',
 }
+
+export const LOGIN_CALLBACK_PATH = '/auth/signed-in'
