3bc4c644497fe040212b1fec2b47f993cd9d48b0
Steve Lee <steve@opendirective.com>
2018-02-09 21:57:25 +0000

    Add reboot.css

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import './reboot.css'
 import './index.css'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
