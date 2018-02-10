1fc619d604e345a982c158027b63b0641ed667be
Steve Lee <steve@opendirective.com>
2018-02-10 09:42:14 +0000

    Refactor actions code

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -3,19 +3,34 @@ import { withRouter } from 'react-router-dom'
 
 import auth from '../auth/auth'
 
-const Button = withRouter(({ id, image, label, action, history, ...props }) => {
-  const actionFn =
-    action && action.verb === 'route'
-      ? () => history.push(action.route)
-      : action && action.verb === 'goback' ? () => history.goBack() : () => {}
-  return (
-    <button {...props} type="button" key={id} onClick={actionFn}>
-      {image ? <img className="button-image" src={image} alt="" /> : ''}
-      {image && label ? <br /> : ''}
-      {label ? <span className="button-label">{label}</span> : ''}
-    </button>
+function selectAction(actions, action) {
+  const noop = () => {}
+  const verb = action ? action.verb : 'default'
+  const actionsWithDef = { ...actions, default: noop }
+  return actionsWithDef[verb] || noop
+}
+
+const buttonAction = (action, history) =>
+  selectAction(
+    {
+      route: () => history.push(action.route),
+      goback: () => history.goBack(),
+    },
+    action
   )
-})
+
+const Button = withRouter(
+  ({ id, image, label, action, history, staticContext: unused, ...props }) => {
+    const actionFn = buttonAction(action, history)
+    return (
+      <button {...props} type="button" key={id} onClick={actionFn}>
+        {image ? <img className="button-image" src={image} alt="" /> : ''}
+        {image && label ? <br /> : ''}
+        {label ? <span className="button-label">{label}</span> : ''}
+      </button>
+    )
+  }
+)
 
 const AuthButton = ({ onAuthChanged, ...props }) => {
   return (
