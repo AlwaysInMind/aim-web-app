f4b91c775863c67f77318bc7b392a5a621caf315
Steve Lee <steve@opendirective.com>
2018-03-13 12:28:01 +0000

    Refactor callProvider and Stop UnhandledRejection error. Fixes #4

--- api/src/providers/index.js
+++ api/src/providers/index.js
@@ -11,34 +11,47 @@ const { demoProvider } = require('./demo')
 
 function provider(name) {
   const map = new Map([
-    ['auth0', demoProvider],
+    ['demo', demoProvider],
     ['google-oauth2', googleProvider],
   ])
   return map.get(name)
 }
 
 function providerFunction(providerName, fnName) {
+  console.info(providerName, fnName)
   return provider(providerName)[fnName]
 }
+
+const callProvider = async (fnName, req, res, wrapped) => {
+  let obj
+  try {
+    // Get a token to access the admin API
+    const { accessToken, userId: auth0UserId } = await getIdPIdent(req.user.sub)
+    const [provName, userId] = auth0UserId.split('|')
+    const providerName = provName === 'auth0' ? 'demo' : provName
+    const fn = async (...args) =>
+      providerFunction(providerName, fnName)(accessToken, userId, ...args)
+    obj = await wrapped(fn, req, res)
+  } catch (err) {
+    throw createError(400, err.message, err)
+  }
+  return obj
+}
+
+exports.callProvider = callProvider
 
 exports.handleProvider = (fnName, wrapped) => {
   return withAuth0(async (req, res) => {
     if (!req.user) {
       throw createError(400, 'Problem with Authorization token') // TODO - fix unhandled rejection
-    } // Get a token to access the admin API
+    }
     let obj
     try {
-      const { accessToken, userId: auth0UserId } = await getIdPIdent(
-        req.user.sub
-      )
-      const [providerName, userId] = auth0UserId.split('|')
-      const fn = async (...args) =>
-        providerFunction(providerName, fnName)(accessToken, userId, ...args)
-      obj = await wrapped(fn, req.params)
+      obj = await callProvider(fnName, req, res, wrapped)
     } catch (err) {
-      throw createError(400, err.message, err)
+      send(res, err.statusCode, err.message)
+      return
     }
-
     res.setHeader('Content-Type', 'application/json')
     send(res, 200, obj)
   })
@@ -55,7 +68,8 @@ exports.handlePreferences = (action, wrapped) => {
       const fn = async prefs => prefsFunction(req.user.sub, prefs)
       preferences = await wrapped(fn, req, res)
     } catch (err) {
-      throw createError(400, err.message, err)
+      send(res, err.statusCode, err.message)
+      return
     }
     res.setHeader('Content-Type', 'application/json')
     send(res, 200, preferences)
