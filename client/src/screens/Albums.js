50b054a58f093c9ea7e748897aa7dca4dba4b22f
Steve Lee <steve@opendirective.com>
2018-04-09 21:31:59 +0100

    Fix logout button

--- client/src/screens/Albums.js
+++ client/src/screens/Albums.js
@@ -6,6 +6,8 @@ import { RouterButton } from '../components/Button'
 import { Screen } from '../components/Screen'
 import { preferences } from '../drivers/preferences'
 
+//import './Albums.css'
+
 const AlbumButton = ({ id, title, thumbnail, ...props }) => (
   <RouterButton
     style={{ gridColumn: 'span 2' }}
@@ -20,31 +22,35 @@ const AlbumButton = ({ id, title, thumbnail, ...props }) => (
 const isDefaultAlbumName = name =>
   name.toLowerCase() === preferences.defaultAlbumName.toLowerCase()
 
-const AlbumsScreen = ({ data, ...props }) => (
-  <Screen
-    title="Choose Photo Album"
-    loadingText="Loading your albums..."
-    errorText="Unable to get albums"
-    screenHelpText="Press an album button to view the photos."
-    {...props}
-  >
-    {helpFn =>
-      // closure so can access data prop
-      data
-        .filter(item => !isDefaultAlbumName(item.title))
-        .map(item => (
-          <AlbumButton
-            className="button-router"
-            id={item.id}
-            thumbnail={item.thumbnail}
-            title={item.title}
-            key={item.id}
-            helpFn={helpFn}
-          />
-        ))
-    }
-  </Screen>
-)
+const AlbumsScreen = ({ data, ...props }) => {
+  //  const { complexity } = preferences
+
+  return (
+    <Screen
+      title="Choose Photo Album"
+      loadingText="Loading your albums..."
+      errorText="Unable to get albums"
+      screenHelpText="Press an album button to view the photos."
+      {...props}
+    >
+      {helpFn =>
+        // closure so can access data prop
+        data
+          .filter(item => !isDefaultAlbumName(item.title))
+          .map(item => (
+            <AlbumButton
+              className="button-router"
+              id={item.id}
+              thumbnail={item.thumbnail}
+              title={item.title}
+              key={item.id}
+              helpFn={helpFn}
+            />
+          ))
+      }
+    </Screen>
+  )
+}
 
 const wrappedScreen = withAuth(withFetchJSON(AlbumsScreen, '/api/albums'))
 export { wrappedScreen as AlbumsScreen }
