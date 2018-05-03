5d8320c6f7b1b638f50e00a2a75c6270030fcf59
Steve Lee <steve@opendirective.com>
2018-05-03 18:20:22 +0100

    Added error boundary. Fixes #67

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Switch } from 'react-router-dom'
 
 import { PublicRoute, PrivateRoute } from './AuthRoute'
+import { ErrorBoundary } from '../components/ErrorBoundary'
 import { CallbackScreen } from '../screens/Callback'
 import { HomeScreen } from '../screens/Home'
 import { ActivitiesScreen } from '../screens/Activities'
@@ -13,7 +14,7 @@ import { VideosScreen } from '../screens/Videos'
 import { InfoScreen } from '../screens/Info'
 import { PreferencesScreen } from '../screens/Preferences'
 import { auth } from '../drivers/auth'
-import { fetchPreferences } from '../drivers/preferences'
+import { fetchPreferences, preferences } from '../drivers/preferences'
 
 import './App.css'
 import './Theme.css'
@@ -25,117 +26,114 @@ const handleAuthentication = ({ location }) => {
   return Promise.reject()
 }
 
-const Oops = ({ location }) => (
-  <div>
-    <p>
-      Something a little unusual happended and I'm not sure what to show you for
-      "{`${location.pathname}${location.search}${location.hash}`}"
-    </p>
-    <p>
-      Please try going back or restarting and do let the AlwaysInMind Team know
-      what was happening so we can investigate. Thanks.
-    </p>
-  </div>
-)
-
 export class App extends React.Component {
   render() {
+    const alertUser = preferences && preferences.complexity >= 2
+
     return (
       <Router>
-        <Switch>
-          <PublicRoute
-            path="/login"
-            render={({ location }) => {
-              auth.login()
-              return null // auth response will route to auth.loginCallbackRoute
-            }}
-          />
+        <ErrorBoundary alertUser={alertUser}>
+          <Switch>
+            <PublicRoute
+              path="/login"
+              render={({ location }) => {
+                auth.login()
+                return null // auth response will route to auth.loginCallbackRoute
+              }}
+            />
 
-          <PublicRoute
-            path={auth.loginCallbackRoute}
-            render={props => {
-              handleAuthentication(props)
-                .then(() => {
-                  fetchPreferences().then(props.history.replace('/'))
-                })
-                .catch(error => {
-                  console.error(`Problem processing Auth0 redirect ${error}`)
-                })
-              return <CallbackScreen {...props} /> // display untill callback reroutes
-            }}
-          />
-          <PrivateRoute
-            exact
-            path="/"
-            render={() => {
-              return <HomeScreen />
-            }}
-          />
-          <PrivateRoute
-            exact
-            path="/activities"
-            render={() => {
-              return <ActivitiesScreen />
-            }}
-          />
-          <PrivateRoute
-            exact
-            path="/photos"
-            render={() => {
-              return <ChoosePhotosScreen />
-            }}
-          />
-          <PrivateRoute
-            exact
-            path="/videos"
-            render={() => {
-              return <ChooseVideosScreen />
-            }}
-          />
-          <PrivateRoute
-            exact
-            path="/info"
-            render={() => {
-              return <ChooseInfoScreen />
-            }}
-          />
-          <PrivateRoute
-            path="/photos/:id"
-            render={({
-              match: {
-                params: { id },
-              },
-            }) => <PhotosScreen fetchURLProps={{ id }} />}
-          />
-          <PrivateRoute
-            path="/videos/:id"
-            render={({
-              match: {
-                params: { id },
-              },
-            }) => <VideosScreen playlistId={id} />}
-          />
-          <PrivateRoute
-            path="/info/:url"
-            render={({
-              match: {
-                params: { url },
-              },
-            }) => (
-              <InfoScreen
-                url={decodeURIComponent(url)}
-                fetchURLProps={{ url }}
-              />
-            )}
-          />
-          <PrivateRoute
-            path="/preferences"
-            render={() => <PreferencesScreen />}
-          />
-          <PublicRoute
-            render={({ location }) => <Oops location={location} />}
-          />
-        </Switch>
+            <PublicRoute
+              path={auth.loginCallbackRoute}
+              render={props => {
+                handleAuthentication(props)
+                  .then(() => {
+                    fetchPreferences().then(props.history.replace('/'))
+                  })
+                  .catch(error => {
+                    console.error(`Problem processing Auth0 redirect ${error}`)
+                  })
+                return <CallbackScreen {...props} /> // display untill callback reroutes
+              }}
+            />
+            <PrivateRoute
+              exact
+              path="/"
+              render={() => {
+                return <HomeScreen />
+              }}
+            />
+            <PrivateRoute
+              exact
+              path="/activities"
+              render={() => {
+                return <ActivitiesScreen />
+              }}
+            />
+            <PrivateRoute
+              exact
+              path="/photos"
+              render={() => {
+                return <ChoosePhotosScreen />
+              }}
+            />
+            <PrivateRoute
+              exact
+              path="/videos"
+              render={() => {
+                return <ChooseVideosScreen />
+              }}
+            />
+            <PrivateRoute
+              exact
+              path="/info"
+              render={() => {
+                return <ChooseInfoScreen />
+              }}
+            />
+            <PrivateRoute
+              path="/photos/:id"
+              render={({
+                match: {
+                  params: { id },
+                },
+              }) => <PhotosScreen fetchURLProps={{ id }} />}
+            />
+            <PrivateRoute
+              path="/videos/:id"
+              render={({
+                match: {
+                  params: { id },
+                },
+              }) => <VideosScreen playlistId={id} />}
+            />
+            <PrivateRoute
+              path="/info/:url"
+              render={({
+                match: {
+                  params: { url },
+                },
+              }) => (
+                <InfoScreen
+                  url={decodeURIComponent(url)}
+                  fetchURLProps={{ url }}
+                />
+              )}
+            />
+            <PrivateRoute
+              path="/preferences"
+              render={() => <PreferencesScreen />}
+            />
+            <PublicRoute
+              render={({ location }) => {
+                throw new Error(
+                  `Unknown route: ${location.pathname}${location.search}${
+                    location.hash
+                  }`
+                )
+              }}
+            />
+          </Switch>
+        </ErrorBoundary>
       </Router>
     )
   }
