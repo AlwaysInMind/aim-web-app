40f376c1f8d55df3b1a1bbc3232ff318858558ee
Steve Lee <steve@opendirective.com>
2018-05-15 06:57:31 +0100

    Add descriptions to demo photos

--- api/src/providers/demo/photos.js
+++ api/src/providers/demo/photos.js
@@ -27,6 +27,7 @@ const dogAlbum = [
     src:
       'https://upload.wikimedia.org/wikipedia/commons/8/8a/Too-cute-doggone-it-video-playlist.jpg',
     medium: 'image',
+    description: 'a brown and white dog carrying a frisbee in its mouth',
   },
   {
     id: '101',
@@ -34,6 +35,7 @@ const dogAlbum = [
     src:
       'https://images.pexels.com/photos/36436/dalmatians-dog-animal-head.jpg?w=1260&h=750&dpr=2&auto=compress&cs=tinysrgb',
     medium: 'image',
+    description: 'a close up of a dog staring at the camera',
   },
   {
     id: '102',
@@ -41,6 +43,7 @@ const dogAlbum = [
     src:
       'https://www.videvo.net/videvo_files/converted/2017_09/preview/170804_A_Lombok_002.mp499448.webm',
     medium: 'video',
+    description: 'a close up of a dog staring at the camera',
   },
 
   {
@@ -48,6 +51,7 @@ const dogAlbum = [
     title: 'African Wild Dog',
     src: 'https://c1.staticflickr.com/2/1563/25934660276_98776b43cd_b.jpg',
     medium: 'image',
+    description: 'a dog looking at the camera',
   },
 ]
 
@@ -57,6 +61,7 @@ const catAlbum = [
     title: 'Relaxed',
     src: 'https://static.pexels.com/photos/22346/pexels-photo.jpg',
     medium: 'image',
+    description: 'a cat lying on the ground',
   },
   {
     id: '201',
@@ -64,6 +69,7 @@ const catAlbum = [
     src:
       'https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/CatVibrissaeFullFace.JPG/2880px-CatVibrissaeFullFace.JPG',
     medium: 'image',
+    description: 'a close up of a cat',
   },
 
   {
@@ -71,6 +77,7 @@ const catAlbum = [
     title: 'Kittens',
     src: 'https://tctechcrunch2011.files.wordpress.com/2008/01/cat_0160.jpg',
     medium: 'image',
+    description: 'a cat lying on a blue blanket',
   },
 
   {
@@ -79,6 +86,7 @@ const catAlbum = [
     src:
       'https://ak4.picdn.net/shutterstock/videos/5724974/preview/stock-footage-cheetah-running.mp4',
     medium: 'video',
+    description: 'a cat lying on a blue blanket',
   },
 ]
 
